184581bf50ea9989e8d7c03fbc8b91fc25ccd7dc
Alain Véron MANIRAMBONA <alainveron.manirambona@obr.gov.bi>
2025-11-03 10:19:52 +0200

    initializ project and configuration

---
+++ src/test/java/com/veron/spoclo_back/SpocloBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veron.spoclo_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpocloBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
